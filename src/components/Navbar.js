4e8764fce7273e004e1db5427111822af3d721e8
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2024-05-31 16:35:29 -0500

    Fixed clickable navbar and centered logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,27 @@
 import { Link, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import LppLogo from "../lpp_logo.png"
 
 function Navbar() {
+    const [visible, setVisible] = useState(false)
+    const [cssSelection, setCssSelection] = useState('NavbarList')
+    function changeListVisibility() {
+        setVisible(!visible) 
+    }
+    useEffect(() => {
+        if(visible)
+            setCssSelection('NavbarListClicked')
+        else
+            setCssSelection('NavbarList')
+    }, [visible])
     return(
         <div className='Navbar'>
             <div className='Sidebar'>
-                <img src={LppLogo} className="LppLogo"/>
-                <div className='NavbarList'>
+                <div className="LogoNav">
+                    <img src={LppLogo} className="LppLogo"/>
+                    <button onClick={changeListVisibility} className="Navigate">Navigate</button>
+                </div> 
+                <div className={cssSelection}>
                     <Link className='NavbarEntry' to='/'>Home</Link> 
                     <Link className='NavbarEntry' to='/about'>About</Link>
                     <Link className='NavbarEntry' to='/ashland'>Building</Link>
