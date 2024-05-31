61b2b2fadee0104a970b03e5241a0e964291346c
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2024-05-31 16:13:22 -0500

    Added logo centered on banner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import { Link, Outlet } from "react-router-dom";
+import LppLogo from "../lpp_logo.png"
 
 function Navbar() {
     return(
         <div className='Navbar'>
             <div className='Sidebar'>
-                <div className='Logo'>Cool LPP Logo</div>
+                <img src={LppLogo} className="LppLogo"/>
                 <div className='NavbarList'>
                     <Link className='NavbarEntry' to='/'>Home</Link> 
                     <Link className='NavbarEntry' to='/about'>About</Link>
