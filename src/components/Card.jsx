8bb57432d36ce712c4fca1b622b5a9e639c9ccbc
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2024-06-01 16:14:25 -0500

    Fixed unnecessary text coloring on ashland card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom"
+
 function Card({ cardClass, image, title, description}) {
     return(
-        <div className={cardClass + "Card"}>
-            <img src={image} alt="" className={cardClass + "Image"}/>
-            <div className={cardClass + "Desc"}>
-                <p>{title}</p>
-                <p>{description}</p>
+        <Link className="RemoveDecoration" to="/ashland">
+            <div className={cardClass + "Card"}>
+                    <img src={image} alt="" className={cardClass + "Image"}/>
+                    <div className={cardClass + "Desc"}>
+                        <p>{title}</p>
+                        <p>{description}</p>
+                    </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
