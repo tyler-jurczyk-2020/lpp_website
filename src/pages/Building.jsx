ea1f264e2e5e873954700d51651882f18a19eeb4
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2024-06-02 23:49:41 -0500

    Added Units Title

--- src/pages/Building.jsx
+++ src/pages/Building.jsx
@@ -12,18 +12,21 @@ function Building({ address, thumbnails, areaDescription }) {
     return(
         <div>
             <p className="BuildingAddy">{address}</p>
-            <div className="Floors">
-                <div className="FloorsCards">
-                <Card cardClass="Ashland" image={thumbnails[0]} target="/ashland/firstfloor" 
-                      title="First Floor Duplex 2 Bed 1 Bath" description="It will cost your kidney to live here"/>
-                </div>
-                <div className="FloorsCards">
-                <Card cardClass="Ashland" image={thumbnails[1]} target="/ashland/secondfloor"
-                      title="Second Floor 2 Bed 1 Bath" description="Party central"/>
-                </div>
-                <div className="FloorsCards">
-                <Card cardClass="Ashland" image={thumbnails[2]} target="/ashland/thirdfloor"
-                      title="Third Floor 2 Bed 1 Bath" description="Dishwasher is crashing out hard"/>
+            <div className="Units">
+                <p className="LocalArea">Units</p>
+                <div className="Floors">
+                    <div className="FloorsCards">
+                    <Card cardClass="Ashland" image={thumbnails[0]} target="/ashland/firstfloor" 
+                          title="First Floor Duplex 2 Bed 1 Bath" description="It will cost your kidney to live here"/>
+                    </div>
+                    <div className="FloorsCards">
+                    <Card cardClass="Ashland" image={thumbnails[1]} target="/ashland/secondfloor"
+                          title="Second Floor 2 Bed 1 Bath" description="Party central"/>
+                    </div>
+                    <div className="FloorsCards">
+                    <Card cardClass="Ashland" image={thumbnails[2]} target="/ashland/thirdfloor"
+                          title="Third Floor 2 Bed 1 Bath" description="Dishwasher is crashing out hard"/>
+                    </div>
                 </div>
             </div>
             <div>
