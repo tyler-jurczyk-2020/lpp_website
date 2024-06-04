f620e778e3ed90d182d024fe1c63ddf074847fc5
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2024-06-03 23:58:07 -0500

    Moved floor numbers to top for readability

--- src/pages/Building.jsx
+++ src/pages/Building.jsx
@@ -4,32 +4,43 @@ import Card from "../components/Card";
 function Building({ address, thumbnails, areaDescription }) {
     const [toggleDiv, setToggleDiv] = useState("HideDiv")
     function ToggleDiv() {
-        if(toggleDiv == "")
+        if (toggleDiv == "")
             setToggleDiv("HideDiv")
         else
             setToggleDiv("")
     }
-    return(
+    return (
         <div>
             <p className="BuildingAddy">{address}</p>
             <div className="Units">
                 <p className="LocalArea">Units</p>
                 <div className="Floors">
+
                     <div className="CardFloor">
                         <p className="FloorNumber">First Floor</p>
                         <div className="FloorsCards">
-                        <Card cardClass="Unit" image={thumbnails[0]} target="/ashland/firstfloor" 
-                              title="Duplex 2 Bed 1 Bath" description="Available July 1st"/>
+                            <Card cardClass="Unit" image={thumbnails[0]} target="/ashland/firstfloor"
+                                title="Duplex 2 Bed 1 Bath" description="Available July 1st" />
                         </div>
                     </div>
-                    <div className="FloorsCards">
-                    <Card cardClass="Unit" image={thumbnails[1]} target="/ashland/secondfloor"
-                          title="2 Bed 1 Bath" description="Unavailable"/>
+
+                    <div className="CardFloor">
+
+                        <p className="FloorNumber">Second Floor</p>
+                        <div className="FloorsCards">
+                            <Card cardClass="Unit" image={thumbnails[1]} target="/ashland/secondfloor"
+                                title="2 Bed 1 Bath" description="Unavailable" />
+                        </div>
                     </div>
-                    <div className="FloorsCards">
-                    <Card cardClass="Unit" image={thumbnails[2]} target="/ashland/thirdfloor"
-                          title="2 Bed 1 Bath" description="Unavailable"/>
+
+                    <div className="CardFloor">
+                        <p className="FloorNumber">Third Floor</p>
+                        <div className="FloorsCards">
+                            <Card cardClass="Unit" image={thumbnails[2]} target="/ashland/thirdfloor"
+                                title="2 Bed 1 Bath" description="Unavailable" />
+                        </div>
                     </div>
+
                 </div>
             </div>
             <div>
