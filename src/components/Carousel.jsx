34d38732e4f861b2c13034bfb673fe0333403af5
Tyler Jurczyk <tylerjurczyk2020@gmail.com>
2024-06-28 18:30:22 -0500

    Caroseul buttons now navigate images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,14 +2,13 @@ import { useState } from "react"
 
 function Carousel({ imageSet }) {
     const [index, setIndex] = useState(0)
-    console.log(imageSet)
     return (
         <div className="CarouselMain">
-            <div className="CarouselNextWrapper">
+            <div className="CarouselNextWrapper" onClick={() => (index > 0) ? setIndex(index - 1) : _}>
                 <p className="CarouselNext">&lt;</p>
             </div>
             <img className="CarouselImg" src={imageSet[index]} alt=""/> 
-            <div className="CarouselPrevWrapper">
+            <div className="CarouselPrevWrapper" onClick={() => setIndex(index + 1)}>
                 <p className="CarouselPrev">&gt;</p>
             </div>
         </div>
